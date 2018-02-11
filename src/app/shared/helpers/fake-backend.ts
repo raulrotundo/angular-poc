a4de64bfe4e21fba284a37acba30ca218ca1243e
Raul Rotundo <raulrotundo@gmail.com>
2018-02-11 15:36:03 -0300

    Refactor FakeBackendInterceptor in Angular 5

--- src/app/shared/helpers/fake-backend.ts
+++ src/app/shared/helpers/fake-backend.ts
@@ -1,75 +1,70 @@
-import { Http, BaseRequestOptions, Response, ResponseOptions, RequestMethod, XHRBackend, RequestOptions } from '@angular/http';
-import { MockBackend, MockConnection } from '@angular/http/testing';
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/materialize';
+import 'rxjs/add/operator/dematerialize';
 import { USERS } from '../mock/users';
 
-export function mockBackEndFactory(backend: MockBackend, options: BaseRequestOptions, realBackend: XHRBackend) {
+@Injectable()
+export class FakeBackendInterceptor implements HttpInterceptor {
+
+    constructor() { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // Mock array for registered users
         const users = USERS;
 
-        // configure fake backend
-        backend.connections.subscribe((connection: MockConnection) => {
-            // wrap in timeout to simulate server api call
-            setTimeout(() => {
-                // authenticate
-                if (connection.request.url.endsWith('/api/authenticate') && connection.request.method === RequestMethod.Post) {
-                    // get parameters from post request
-                    const params = JSON.parse(connection.request.getBody());
+        // wrap in delayed observable to simulate server api call
+        return Observable.of(null).mergeMap(() => {
 
-                    // find if any user matches login credentials
-                    const filteredUsers = users.filter(user => {
-                        return user.username === params.username && user.password === params.password;
-                    });
+            // authenticate
+            if (request.url.endsWith('/api/authenticate') && request.method === 'POST') {
 
-                    if (filteredUsers.length) {
-                        // if login details are valid return 200 OK with user details and fake jwt token
-                        const user = filteredUsers[0];
-                        connection.mockRespond(new Response(new ResponseOptions({
-                            status: 200,
-                            body: {
-                                id: user.id,
-                                username: user.username,
-                                firstName: user.firstName,
-                                lastName: user.lastName,
-                                token: 'fake-jwt-token'
-                            }
-                        })));
-                    } else {
-                        // else return 400 bad request
-                        connection.mockError(new Error('Username or password is incorrect'));
-                    }
-
-                    return;
-                }
-
-                // pass through any requests not handled above
-                const realHttp = new Http(realBackend, options);
-                const requestOptions = new RequestOptions({
-                    method: connection.request.method,
-                    headers: connection.request.headers,
-                    body: connection.request.getBody(),
-                    url: connection.request.url,
-                    withCredentials: connection.request.withCredentials,
-                    responseType: connection.request.responseType
+                // find if any user matches login credentials
+                const filteredUsers = users.filter(user => {
+                    return user.username === request.body.username && user.password === request.body.password;
                 });
 
-                realHttp.request(connection.request.url, requestOptions)
-                    .subscribe((response: Response) => {
-                        connection.mockRespond(response);
-                    },
-                    (error: any) => {
-                        connection.mockError(error);
+                if (filteredUsers.length) {
+                    // if login details are valid return 200 OK with user details and fake jwt token
+                    const user = filteredUsers[0];
+                    const body = {
+                        id: user.id,
+                        username: user.username,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        token: 'fake-jwt-token'
+                    }
+
+                    // respond 200 OK
+                    return Observable.of(new HttpResponse({ status: 200, body: body }));
+                } else {
+                    // else return 400 bad request
+                    return Observable.throw({
+                        error: {
+                            errors: {
+                                msg: 'Authentication failed. User not found.'
+                            }
+                        }
                     });
+                };
+            }
 
-            }, 1000);
+            // pass through any requests not handled above
+            return next.handle(request);
 
-        });
-
-        return new Http(backend, options);
+        }).materialize().delay(500).dematerialize();
+        // call materialize and dematerialize to ensure delay even if an error is thrown
     }
+}
 
 export let fakeBackendProvider = {
     // use fake backend in place of Http service for backend-less development
-    provide: Http,
-    deps: [MockBackend, BaseRequestOptions, XHRBackend],
-    useFactory: mockBackEndFactory
+    provide: HTTP_INTERCEPTORS,
+    useClass: FakeBackendInterceptor,
+    multi: true
 };
